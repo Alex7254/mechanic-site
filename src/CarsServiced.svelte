f1c31fe676cee0f574cefaea959a7f6d61416744
Alejandro <alejandro199898@gmail.com>
2021-10-07 13:57:47 -0700

    re-factor code

--- src/CarsServiced.svelte
+++ src/CarsServiced.svelte
@@ -3,40 +3,42 @@
   import Service from "./Service.svelte";
 </script>
 
+<section class="cars-serviced">
+  <PistonHeader title="Brands We Service" />
+  <div class="cars-serviced__gallery">
+    <img class="cars-serviced__logo img-padding-bottom"src="/assets/volkswagen.png" alt=""/>
+    <img class="cars-serviced__logo" src="/assets/honda.png" alt="" />
+    <img class="cars-serviced__logo" src="/assets/ford.png" alt="" />
+    <img class="cars-serviced__logo" src="/assets/nissan.png" alt="" />
+    <img class="cars-serviced__logo" src="/assets/dodge.png" alt="" />
+    <img class="cars-serviced__logo" src="/assets/subaru.png" alt="" />
+  </div>
+</section>
+
 <style>
-  #gallery-container {
+  .cars-serviced__gallery {
     /* background-color: lightslategray; */
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: space-evenly;
-    padding: 1rem 8%;
+    padding: 1rem;
   }
 
-  img {
+  .cars-serviced__logo {
     height: 8rem;
     display: inline-block;
     border-bottom: solid 2px white;
     /* background-color: cornflowerblue; */
   }
 
-  img:hover {
+  .cars-serviced__logo:hover {
     border-bottom: solid 2px red;
     transition: all 0.6s ease-in-out;
   }
 
-  #volkswagen-img {
+  .img-padding-bottom {
     padding-bottom: 1rem;
     height: 6.5rem;
   }
 </style>
-
-<PistonHeader title="Brands We Service" />
-<div id="gallery-container">
-  <img src="/assets/volkswagen.png" alt="" id="volkswagen-img" />
-  <img src="/assets/honda.png" alt="" />
-  <img src="/assets/ford.png" alt="" />
-  <img src="/assets/nissan.png" alt="" />
-  <img src="/assets/dodge.png" alt="" />
-  <img src="/assets/subaru.png" alt="" />
-</div>

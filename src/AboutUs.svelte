385df1f72dfed28c52e836d665b7403b8059b634
Alejandro <alejandro199898@gmail.com>
2021-10-07 13:59:02 -0700

    change file structure

--- src/AboutUs.svelte
+++ src/AboutUs.svelte
@@ -1,6 +1,34 @@
 <script>
-
 </script>
+
+<div class="blue-gradient about-container">
+  <div class="about-content">
+    <h1>About Us</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo sit non
+      corporis optio quod possimus numquam voluptatem earum molestiae
+      accusantium excepturi totam, aut incidunt veniam itaque distinctio aliquam
+      culpa sequi neque quam vero vitae officiis dolorum facilis. Tempore dolore
+      id fuga pariatur praesentium labore perspiciatis, deserunt obcaecati
+      deleniti beatae itaque assumenda sunt voluptates, doloribus modi impedit
+      <br />
+      dolorum. Pariatur aperiam, quae nostrum enim corrupti perspiciatis maiores
+      sit amet, harum expedita quas voluptates possimus perferendis! Ratione deleniti
+      ipsa minima pariatur porro distinctio nisi, laudantium corporis laboriosam
+      inventore quibusdam, rem quis dolorum repellat nobis a quam qui temporibus.
+      Reiciendis, deleniti rerum.
+    </p>
+    <div class="diagonal-button">
+      <span class="button-slanted-content">Contact Us</span>
+    </div>
+  </div>
+  <div class="img-container">
+    <img
+      src="https://images.unsplash.com/photo-1530046339160-ce3e530c7d2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+      alt=""
+    />
+  </div>
+</div>
 
 <style>
   .about-container {
@@ -16,7 +44,7 @@
   .about-content {
     color: white;
     /* background-color: green; */
-    padding:0 1rem;
+    padding: 0 1rem;
     width: 35vw;
   }
 
@@ -45,31 +73,3 @@
     height: auto;
   }
 </style>
-
-<div class="blue-gradient about-container">
-  <div class="about-content">
-    <h1>About Us</h1>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo sit non
-      corporis optio quod possimus numquam voluptatem earum molestiae
-      accusantium excepturi totam, aut incidunt veniam itaque distinctio aliquam
-      culpa sequi neque quam vero vitae officiis dolorum facilis. Tempore dolore
-      id fuga pariatur praesentium labore perspiciatis, deserunt obcaecati
-      deleniti beatae itaque assumenda sunt voluptates, doloribus modi impedit
-      <br />
-      dolorum. Pariatur aperiam, quae nostrum enim corrupti perspiciatis maiores
-      sit amet, harum expedita quas voluptates possimus perferendis! Ratione
-      deleniti ipsa minima pariatur porro distinctio nisi, laudantium corporis
-      laboriosam inventore quibusdam, rem quis dolorum repellat nobis a quam qui
-      temporibus. Reiciendis, deleniti rerum.
-    </p>
-    <div class="diagonal-button">
-      <span class="button-slanted-content">Contact Us</span>
-    </div>
-  </div>
-  <div class="img-container">
-    <img
-      src="https://images.unsplash.com/photo-1530046339160-ce3e530c7d2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-      alt="" />
-  </div>
-</div>

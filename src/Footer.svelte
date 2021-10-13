cff68282e94eb9dbed69407a57ab5e55831d7bfe
Alejandro <alejandro199898@gmail.com>
2021-10-13 16:05:47 -0700

    edit footer item spacing and size

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,34 +1,40 @@
 <script>
-
 </script>
 
-<style>
-  footer {
-    padding: 3rem 6%;
-  }
-  li {
-    color: white;
-    font-size: 0.9rem;
-    font-weight: 200;
-  }
-</style>
-
-<footer class="blue-gradient">
-  <ul>
-    <li>
+<footer class="footer blue-gradient">
+  <ul class="footer__nav">
+    <li class="footer__nav__item">
       <a href="/">Home</a>
     </li>
-    <li>
+    <li class="footer__nav__item">
       <a href="/">Services</a>
     </li>
-    <li>
+    <li class="footer__nav__item">
       <a href="/">About</a>
     </li>
-    <li>
+    <li class="footer__nav__item">
       <a href="/">Contact</a>
     </li>
-    <li>
+    <li class="footer__nav__item designer-tag">
       <p class="light-text">Designed by Me</p>
     </li>
   </ul>
 </footer>
+
+<style>
+  .footer {
+    padding: 3rem 6%;
+  }
+  .footer__nav__item {
+    color: white;
+    margin: 0.65rem;
+    transition: color 0.4s ease-in-out;
+  }
+  .footer__nav__item:hover {
+    color: var(--red);
+  }
+  .designer-tag {
+    margin-top: 2rem;
+    cursor: default;
+  }
+</style>

9df46f81266e9619ed1b40d2cea2f6f37bdbac87
Alejandro <alejandro199898@gmail.com>
2021-10-13 12:00:26 -0700

    edit layout and css

--- src/ContactUs.svelte
+++ src/ContactUs.svelte
@@ -4,18 +4,44 @@
   // import {GoogleMap} from "@beyonk/svelte-googlemaps";
 </script>
 
+<section class="contact-us" id="contact-us">
+  <PistonHeader title="Contact Us" />
+  <div class="contact-us__container d-flex-nw">
+    <div class="contact-us__details blue-gradient">
+      <p>
+        <b>Phone:</b>
+        (831)012-3456
+      </p>
+      <p>
+        <b>Monday-Friday:</b>
+        9:30am-5:00pm
+      </p>
+      <p>
+        <b>Saturday:</b>
+        9:30am-3:00pm
+      </p>
+      <p>
+        <b>
+          123 Union St
+          <br />
+          Watsonville, CA 95076
+        </b>
+      </p>
+    </div>
+    <Map />
+  </div>
+</section>
+
 <style>
-  section {
-    display: flex;
+  .contact-us {
     align-items: center;
     justify-content: space-evenly;
-    flex-wrap: nowrap;
     flex-direction: row;
     padding: 1rem 8%;
   }
-  div {
-    height: 22rem;
-    width: 50vw;
+  .contact-us__details {
+    height: 25rem;
+    width: 50%;
     display: flex;
     justify-content: space-evenly;
     flex-wrap: nowrap;
@@ -23,40 +49,7 @@
     color: white;
     padding: 2rem 4rem;
     border-radius: 10px 0 0 10px;
-  }
-  p {
     font-size: 1.2rem;
-    font-weight: 200;
     letter-spacing: 1.3px;
   }
-  span {
-    font-weight: 700;
-  }
 </style>
-
-<PistonHeader title="Contact Us" />
-
-<section id="contact-us">
-  <div class="blue-gradient">
-    <p>
-      <span>Phone:</span>
-      (831)012-3456
-    </p>
-    <p>
-      <span>Monday-Friday:</span>
-      9:30am-5:00pm
-    </p>
-    <p>
-      <span>Saturday:</span>
-      9:30am-3:00pm
-    </p>
-    <p>
-      <span>
-        123 Union St
-        <br />
-        Watsonville, CA 95076
-      </span>
-    </p>
-  </div>
-  <Map />
-</section>

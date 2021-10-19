0940da747ad4eb7bad2a8ec09fd8e36f040ec51b
Alejandro <alejandro199898@gmail.com>
2021-10-18 19:43:59 -0700

    replace id's with classnames

--- src/Navigation.svelte
+++ src/Navigation.svelte
@@ -1,15 +1,15 @@
 <script>
 </script>
 
-<nav role="navigation">
-  <div id="menuToggle">
+<nav class="nav" role="navigation">
+  <div class="menuToggle">
     <input type="checkbox" />
 
     <span />
     <span />
     <span />
 
-    <ul id="menu" class="blue-gradient">
+    <ul class="menu blue-gradient">
       <li><a href="/">Home</a></li>
       <li><a href="/">Services</a></li>
       <li><a href="/">What We Service</a></li>
@@ -21,55 +21,47 @@
 </nav>
 
 <style>
-  nav {
+  .nav {
     position: fixed;
     z-index: 1;
     height: 100vh;
   }
-  #menuToggle {
+
+  .menuToggle {
     display: block;
     position: relative;
     top: 50px;
     left: 50px;
-
     z-index: 3;
-
     -webkit-user-select: none;
     user-select: none;
   }
 
-  #menuToggle a {
+  .menuToggle a {
     text-decoration: none;
     color: white;
-
     transition: color 0.4s ease-in-out;
   }
 
-  #menuToggle li:hover {
+  .menuToggle li:hover a{
     color: #ef233c;
     font-weight: 400;
   }
 
-  #menuToggle input {
+  .menuToggle input {
     display: block;
     width: 40px;
     height: 32px;
     position: absolute;
     top: -7px;
     left: -5px;
-
     cursor: pointer;
-
-    opacity: 0; /* hide this */
-    z-index: 2; /* and place it over the hamburger */
-
+    opacity: 0; 
+    z-index: 2; 
     -webkit-touch-callout: none;
   }
 
-  /*
- * Just a quick hamburger
- */
-  #menuToggle span {
+  .menuToggle span {
     display: block;
     width: 33px;
     height: 4px;
@@ -77,60 +69,38 @@
     position: relative;
     background: #ef233c;
     border-radius: 3px;
-
     z-index: 1;
-
     transform-origin: 4px 0px;
-
     transition: transform 0.5s cubic-bezier(0.77, 0.2, 0.05, 1),
       background 0.5s cubic-bezier(0.77, 0.2, 0.05, 1), opacity 0.6s ease-in-out;
   }
 
-  #menuToggle span:first-child {
+  .menuToggle span:first-child {
     transform-origin: 0% 0%;
   }
-
-  #menuToggle span:nth-last-child(2) {
+  .menuToggle span:nth-last-child(2) {
     transform-origin: 0% 100%;
   }
-
-  /* 
- * Transform all the slices of hamburger
- * into a crossmark.
- */
-  #menuToggle input:checked ~ span {
+  .menuToggle input:checked ~ span {
     opacity: 1;
     transform: rotate(45deg) translate(-2px, -1px);
     background: #ef233c;
   }
-
-  /*
- * But let's hide the middle one.
- */
-  #menuToggle input:checked ~ span:nth-last-child(3) {
+  .menuToggle input:checked ~ span:nth-last-child(3) {
     opacity: 0;
     transform: rotate(0deg) scale(0.2, 0.2);
   }
-
-  /*
- * Ohyeah and the last one should go the other direction
- */
-  #menuToggle input:checked ~ span:nth-last-child(2) {
+  .menuToggle input:checked ~ span:nth-last-child(2) {
     transform: rotate(-45deg) translate(0, -1px);
   }
 
-  /*
- * Make this absolute positioned
- * at the top left of the screen
- */
-  #menu {
+  .menu {
     position: absolute;
     width: 20rem;
     height: 100vh;
     margin: -77px 0 0 -50px;
     padding: 3rem;
     padding-top: 15rem;
-    /* background: #011936; */
     list-style-type: none;
     -webkit-font-smoothing: antialiased;
     transform-origin: 0% 0%;
@@ -138,7 +108,7 @@
     transition: transform 0.5s cubic-bezier(0.77, 0.2, 0.05, 1);
   }
 
-  #menu li {
+  .menu li {
     padding: 10px 0;
     font-size: 1.2rem;
     font-weight: 200;
@@ -146,7 +116,7 @@
     transition: 0.3s color ease-in-out;
   }
 
-  #menuToggle input:checked ~ ul {
+  .menuToggle input:checked ~ ul {
     transform: none;
   }
 </style>

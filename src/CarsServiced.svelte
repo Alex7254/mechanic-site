1fabbde84c84408fdced24e98c0948b8e654373c
Alejandro <alejandro199898@gmail.com>
2021-10-20 16:19:22 -0700

    edit cars serviced layout

--- src/CarsServiced.svelte
+++ src/CarsServiced.svelte
@@ -6,7 +6,11 @@
 <section class="cars-serviced">
   <PistonHeader title="Brands We Service" />
   <div class="cars-serviced__gallery">
-    <img class="cars-serviced__logo img-padding-bottom"src="/assets/volkswagen.png" alt=""/>
+    <img
+      class="cars-serviced__logo img-padding-bottom"
+      src="/assets/volkswagen.png"
+      alt=""
+    />
     <img class="cars-serviced__logo" src="/assets/honda.png" alt="" />
     <img class="cars-serviced__logo" src="/assets/ford.png" alt="" />
     <img class="cars-serviced__logo" src="/assets/nissan.png" alt="" />
@@ -16,29 +20,30 @@
 </section>
 
 <style>
+  .cars-serviced {
+    max-width: 120rem;
+    padding: 2rem 8%;
+    margin: auto;
+  }
   .cars-serviced__gallery {
-    /* background-color: lightslategray; */
     display: flex;
     flex-direction: row;
     align-items: center;
-    justify-content: space-evenly;
-    padding: 1rem;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    width: 100%;
   }
 
   .cars-serviced__logo {
+    width: 8rem;
     height: 8rem;
+    margin: 1.25rem;
     display: inline-block;
-    border-bottom: solid 2px white;
-    /* background-color: cornflowerblue; */
+    filter: grayscale(100%);
+    transition: all 0.4s ease-in-out;
   }
 
   .cars-serviced__logo:hover {
-    border-bottom: solid 2px red;
-    transition: all 0.6s ease-in-out;
-  }
-
-  .img-padding-bottom {
-    padding-bottom: 1rem;
-    height: 6.5rem;
+    filter: grayscale(0%);
   }
 </style>

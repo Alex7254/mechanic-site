8755b0cf41ca473f9351795c919b86156c94921b
Alejandro <alejandro199898@gmail.com>
2021-10-20 14:38:50 -0700

    add media queries

--- src/MechanicServices.svelte
+++ src/MechanicServices.svelte
@@ -3,28 +3,6 @@
   import Service from "./Service.svelte";
 </script>
 
-<style>
-.services {
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-}
-  .services__grid {
-    /* background-color: silver; */
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-auto-rows: 12rem;
-    padding: 2rem 8%;
-    grid-gap: 3rem;
-  }
-
-  /* Add with to button to fit content on this section of website */
-  .diagonal-button {
-    width:12rem;
-    margin: 4rem 0;
-  }
-</style>
-
 <section class="services d-flex-nw">
   <PistonHeader title="How We Can Help" />
   <div class="services__grid">
@@ -37,3 +15,30 @@
     <span class="button-slanted-content">More Services</span>
   </div>
 </section>
+
+<style>
+  .services {
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+  .services__grid {
+    /* background-color: silver; */
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-auto-rows: 12rem;
+    padding: 2rem 8%;
+    grid-gap: 3rem;
+  }
+
+  /* Add with to button to fit content on this section of website */
+  .diagonal-button {
+    width: 12rem;
+    margin: 4rem 0;
+  }
+
+  /* Extra large devices less than 1440px) */
+  @media only screen and (max-width: 90rem) {
+    
+  }
+</style>

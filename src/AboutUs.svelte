f55e42836b6d8b854482411f832ac913d9f8a0f4
Alejandro <alejandro199898@gmail.com>
2021-10-21 15:40:49 -0700

    add max width

--- src/AboutUs.svelte
+++ src/AboutUs.svelte
@@ -1,46 +1,52 @@
 <script>
 </script>
 
-<section class="about-us blue-gradient d-flex-nw">
-  <div class="about-us__content">
-    <h1 class="about-us__content__title">About Us</h1>
-    <p class="about-us__content__text">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo sit non
-      corporis optio quod possimus numquam voluptatem earum molestiae
-      accusantium excepturi totam, aut incidunt veniam itaque distinctio aliquam
-      culpa sequi neque quam vero vitae officiis dolorum facilis. Tempore dolore
-      id fuga pariatur praesentium labore perspiciatis, deserunt obcaecati
-      deleniti beatae itaque assumenda sunt voluptates, doloribus modi impedit
-      <br />
-      dolorum. Pariatur aperiam, quae nostrum enim corrupti perspiciatis maiores
-      sit amet, harum expedita quas voluptates possimus perferendis! Ratione deleniti
-      ipsa minima pariatur porro distinctio nisi, laudantium corporis laboriosam
-      inventore quibusdam, rem quis dolorum repellat nobis a quam qui temporibus.
-      Reiciendis, deleniti rerum.
-    </p>
-    <div class="diagonal-button">
-      <span class="button-slanted-content">Contact Us</span>
+<section class="about-us blue-gradient">
+  <div class="max-width d-flex-nw">
+    <div class="about-us__content">
+      <h1 class="about-us__content__title">About Us</h1>
+      <p class="about-us__content__text">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo sit non
+        corporis optio quod possimus numquam voluptatem earum molestiae
+        accusantium excepturi totam, aut incidunt veniam itaque distinctio aliquam
+        culpa sequi neque quam vero vitae officiis dolorum facilis. Tempore dolore
+        id fuga pariatur praesentium labore perspiciatis, deserunt obcaecati
+        deleniti beatae itaque assumenda sunt voluptates, doloribus modi impedit
+        <br />
+        dolorum. Pariatur aperiam, quae nostrum enim corrupti perspiciatis maiores
+        sit amet, harum expedita quas voluptates possimus perferendis! Ratione deleniti
+        ipsa minima pariatur porro distinctio nisi, laudantium corporis laboriosam
+        inventore quibusdam, rem quis dolorum repellat nobis a quam qui temporibus.
+        Reiciendis, deleniti rerum.
+      </p>
+      <div class="diagonal-button">
+        <span class="button-slanted-content">Contact Us</span>
+      </div>
     </div>
+    <img
+      class="about-us__img"
+      src="https://images.unsplash.com/photo-1543747255-94b72995ac74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+      alt="mechanic bending over to look under car"
+    />
   </div>
-  <img
-    class="about-us__img"
-    src="https://images.unsplash.com/photo-1543747255-94b72995ac74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-    alt="mechanic bending over to look under car"
-  />
 </section>
 
 <style>
-  .about-us {
-    padding: 6rem 8%;
+  .max-width {
     align-items: center;
     justify-content: space-between;
     flex-direction: row;
+    max-width: 120rem;
+    margin: auto;
+    padding: 6rem 8%;
+  }
+  .about-us {
     cursor: default;
   }
 
   .about-us__content {
     color: white;
-    padding: 0 1rem;
+    margin-right: 2rem;
     width: 40vw;
   }
 

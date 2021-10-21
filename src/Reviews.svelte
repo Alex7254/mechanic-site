c8327caaff388bff4061100badf7f2de5b46fbdc
Alejandro <alejandro199898@gmail.com>
2021-10-21 16:26:43 -0700

    add max-width

--- src/Reviews.svelte
+++ src/Reviews.svelte
@@ -3,17 +3,25 @@
   import Review from "./Review.svelte";
 </script>
 
-<PistonHeader title="Reviews" />
-<div class="reviews-container">
-  <Review reviewerName="Alex Garcia" rating="4.2" />
-  <Review rating="3.5" />
-  <Review rating="4.8" />
+<div class="reviews">
+  <PistonHeader title="Reviews" />
+  <div class="reviews-container">
+    <Review reviewerName="Alex Garcia" rating="4.2" />
+    <Review rating="3.5" />
+    <Review rating="4.8" />
+  </div>
 </div>
 
 <style>
+  .reviews {
+    padding: 1rem 8%;
+    max-width: 120rem;
+    margin: auto;
+  }
+
   .reviews-container {
     /* height: 25rem; */
-    padding: 2rem 8%;
+    /* padding: 2rem 8%; */
     /* background-color: blanchedalmond; */
     display: flex;
     flex-direction: row;

a90017483ff2cb7fc581adeff9af049de7f1450b
Alejandro <alejandro199898@gmail.com>
2021-11-06 16:41:23 -0700

    add z-index

--- src/Map.svelte
+++ src/Map.svelte
@@ -22,6 +22,7 @@
     width: 100%;
     height: 30rem;
     border-radius: 0.625rem;
+    z-index: -1;
   }
   /* Medium devices less than 650px */
   @media only screen and (max-width: 40.625rem) {
